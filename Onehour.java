1e5d2ebfb15d4102d20d3926bcde351ca89e9d99
sinchan143054 <sinchan.143054@gmail.com>
2025-05-14 04:11:28 +0530

    save it

---
+++ Onehour.java
@@ -0,0 +1,13 @@
+public class Onehour
+ {
+    public static void main(String[] args) {
+        int id = 345;
+        String name = "Unnati";
+        int salary = 20000;
+
+        System.out.println("Emp id:" +id);
+        System.out.println("Emp name:" +name);
+        System.out.println("Emp salary:" +salary);
+        System.out.println("Emp salary:" +salary);
+    }
+}
